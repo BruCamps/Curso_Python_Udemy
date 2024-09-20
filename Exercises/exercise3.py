3b0fee4f35d15d0fe5a72db0b4d23a4b517406e6
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-09-20 12:53:38 -0300

    Create exercise3.py

---
+++ Exercises/exercise3.py
@@ -0,0 +1,13 @@
+# Exercício: Utilize condicionais (if e else) e operadores relacionais para fazer com que 
+# o número maior seja exibido primeiro e o menor número em seguida.  
+
+# Modelo:
+# segundo_valor='2' é maior do que primeiro_valor='1'
+
+primeiro_valor = input('\nDigite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor > segundo_valor:  # Verifica se o primeiro valor é maior do que o segundo
+  print(f'\nO {primeiro_valor=} é maior do que o {segundo_valor=}\n')
+else:
+  print(f'\nO {segundo_valor=} é maior do que o {primeiro_valor=}\n')
