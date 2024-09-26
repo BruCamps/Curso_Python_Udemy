a126d4443d3ff8add6f29fdc076332e8df20b511
Bruna Campos <86365466+BruCamps@users.noreply.github.com>
2024-09-26 13:59:35 -0300

    Update and fix comments exercise4.py

--- Exercises/exercise4.py
+++ Exercises/exercise4.py
@@ -1,12 +1,14 @@
 """
 Exercício:
+
 Peça ao usuário para digitar seu nome
 Peça ao usuário para digitar sua idade
+
 Se nome e idade forem digitados:
 Exiba:
-Seu nome é {nome} ok
-Seu nome invertido é {nome invertido} ok
-Seu nome contém (ou não) espaços ok
+Seu nome é {nome}
+Seu nome invertido é {nome invertido}
+Seu nome contém (ou não) espaços
 Seu nome tem {n} letras
 A primeira letra do seu nome é {letra}
 A última letra do seu nome é {letra}
